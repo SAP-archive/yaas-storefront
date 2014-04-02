11ff9b6d88b8bb55f7368f656dc638b8a2894088
Vera Coberley <vera.coberley@hybris.com>
2014-04-02 12:28:06 -0600

    add responsive product Service. Load into main page for now

---
+++ public/js/app/core-resource.js
@@ -0,0 +1,344 @@
+/* Source partially from https://gist.github.com/jelbourn/6276338
+ *
+ */
+
+'use strict';
+
+/** Namespace for the application. */
+var yngApp = {};
+
+/******************************************************************************/
+
+/**
+ * Interface for a model objects used with the api service.
+ * @interface
+ */
+yngApp.ApiModel = function() {
+
+};
+
+/**
+ * Data transformation done after fetching data from the server.
+ * @type {Function}
+ */
+yngApp.ApiModel.prototype.afterLoad = function(){
+
+};
+
+/**
+ * Data transformation done before posting / putting data to the server.
+ * @type {Function}
+ */
+yngApp.ApiModel.prototype.beforeSave = function() {
+
+};
+
+yngApp.ApiModel.prototype.validate = function() {
+
+};
+
+/** Returns true if the current model state is valid. */
+yngApp.ApiModel.prototype.isValid = function() {
+
+};
+
+/** Returns true if the model has not yet been persisted. */
+yngApp.ApiModel.prototype.isNew = function() {
+
+};
+
+/******************************************************************************/
+
+/**
+ * Configuration object for an api endpoint.
+ * @constructor
+ */
+yngApp.ApiEndpointConfig = function() {
+    /**
+     * Map of actions for the endpoint, keyed by action name. An action has a HTTP
+     * method (GET, POST, etc.) as well as an optional set of default parameters.
+     * @type {Object.<string, {method: string, params: Object}>}
+     */
+    this.actions = {};
+
+    /** Map of custom headers to be added for each method, keyed on header name.*/
+    this.customHeaders = {};
+
+    /** Array of custom interceptors to be added.*/
+    this.customInterceptors = {};
+
+    /** Flag indicating if CORS should be enabled.*/
+    this.corsEnabled = false;
+
+    /** The default actions defined for every endpoint. */
+    var defaultActions  = {
+        'get':    { method: 'GET' },
+        'save':   { method: 'POST' },
+        'query':  { method: 'GET', isArray:true },
+        'remove': { method: 'DELETE' },
+        'delete': { method: 'DELETE' },
+        'update': { method: 'PUT' }
+    };
+
+    // Add the default actions to this endpoint.
+    var self = this;
+    angular.forEach(defaultActions, function(alias, method) {
+        self.addHttpAction(alias, method);
+    });
+};
+
+/**
+* Adds an action to the endpoint.
+* @param {string} method The HTTP method for the action.
+* @param {string} name The name of the action.
+* @param {Object=} params The default parameters for the action.
+*/
+yngApp.ApiEndpointConfig.prototype.addHttpAction = function(alias, method, params) {
+    this.actions[name] = {method: method, params: params};
+};
+
+
+/**
+ * Set the route for this endpoint. This is relative to the server's base route.
+ * @param {string} route
+ * @return {yngApp.ApiEndpointConfig}
+ */
+yngApp.ApiEndpointConfig.prototype.route = function(route) {
+    this.route = route;
+    return this;
+};
+
+
+/**
+ * Set the route for this endpoint. This is relative to the server's base route.
+ * @param {function(): yngApp.ApiModel} model
+ * @return {yngApp.ApiEndpointConfig}
+ */
+yngApp.ApiEndpointConfig.prototype.model = function(model) {
+    this.model = model;
+    return this;
+};
+
+yngApp.ApiEndpointConfig.prototype.customHeaders = function (customHeaders) {
+    this.customHeaders = customHeaders;
+    return this;
+};
+
+yngApp.ApiEndpointConfig.prototype.customInterceptors = function (customInterceptors) {
+    this.customInterceptors = customInterceptors;
+    return this;
+};
+
+/** Enables CORS for this endpoint. */
+yngApp.ApiEndpointConfig.prototype.enableCors = function () {
+    this.corsEnabled = true;
+    return this;
+};
+
+
+
+/******************************************************************************/
+
+/**
+ * An api endpoint.
+ *
+ * @constructor
+ * @param {string} baseRoute The server api's base route.
+ * @param {yngApp.ApiEndpointConfig} endpointConfig Configuration object for the
+ *     endpoint.
+ * @param {!Object} $injector The angular $injector service.
+ * @param {!Function} $resource The angular $resource service.
+ */
+yngApp.ApiEndpoint = function(baseRoute, endpointConfig, $injector, $resource) {
+    this.config = endpointConfig;
+    this.$injector = $injector;
+
+    this.API = $resource(baseRoute + endpointConfig.route, {},
+        endpointConfig.actions);
+
+
+
+    // Extend this endpoint objects with methods for all of the actions defined
+    // in the configuration object. The action performed depends on whether or
+    // not there is a model defined in the configuration; when there is a model
+    // defined, certain request types must be wrapped in order to apply the
+    // pre/post request transformations defined by the model.
+    var self = this;
+    angular.forEach(endpointConfig.actions, function(action, actionName) {
+        var actionMethod = self.request;
+
+        /*
+        if (action.model) {
+            if (action.method === 'GET') {
+                actionMethod = self.getRequestWithModel;
+            } else if (action.method === 'PUT' || action.method === 'POST') {
+                actionMethod = self.saveRequestWithModel;
+            }
+        }*/
+
+        action.headers = angular.extend(action.headers || {}, endpointConfig.customHeaders || {});
+
+        // Handle interceptors
+        if (!action.interceptor) {
+            action = angular.extend(action, { interceptor: endpointConfig.customInterceptors });
+        } else {
+            // Merge custom with default interceptors
+            var mergedInterceptor = action.interceptor;
+            angular.forEach(action.interceptor, function(interceptorFn, interceptorKey) {
+                if (endpointConfig.customInterceptors[interceptorKey]) {
+                    mergedInterceptor[interceptorKey] = function() {
+                        endpointConfig.customInterceptors[interceptorKey].apply(this, arguments);
+                        interceptorFn.apply(this, arguments);
+                    };
+                }
+                action.interceptor = mergedInterceptor;
+            });
+        }
+
+        self[actionName] = angular.bind(self, actionMethod, actionName);
+    });
+
+    /*
+    if(endpointConfig.corsEnabled) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }  */
+};
+
+
+/**
+ * Instantiates a model object from the raw server response data.
+ * @param {Object} response The raw server response data.
+ * @return {app.ApiModel} The server response data wrapped in a model object.
+ */
+yngApp.ApiEndpoint.prototype.instantiateModel = function(response) {
+    var model = this.$injector.instantiate(this.config.model);
+    angular.extend(model, response);
+    model.afterLoad();
+
+    return model;
+};
+
+/**
+ * Perform a standard http request.
+ *
+ * @param {string} action The name of the action.
+ * @param {Object=} params The parameters for the request.
+ * @param {Object=} data The request data (for PUT / POST requests).
+ * @return {angular.$q.Promise} A promise resolved when the http request has
+ *     a response.
+ */
+yngApp.ApiEndpoint.prototype.request = function(action, params, data) {
+    return this.API[action](params, data).$promise;
+};
+
+/**
+ * Perform an HTTP GET request and performs a post-response transformation
+ * on the data as defined in the model object.
+ *
+ * @param {string} action The name of the action.
+ * @param {Object=} params The parameters for the request.
+ * @return {angular.$q.Promise} A promise resolved when the http request has
+ *     a response.
+ */
+yngApp.ApiEndpoint.prototype.getRequestWithModel = function(action, params) {
+    return this.request(action, params);
+    //var instantiateModel = this.instantiateModel.bind(this);
+
+    // Wrap the raw server response data in an instantiated model object
+    // (or multiple, if response data is an array).
+    //return promise.then(function(response) {
+
+       //instantiateModel(response);
+        //return response.products;
+    //});
+};
+
+/**
+ * Performs an HTTP PUT or POST after performing a pre-request transformation
+ * on the data as defined in the model object.
+ *
+ * @param {string} action The name of the action.
+ * @param {Object=} params The parameters for the request.
+ * @param {Object=} data The request data (for PUT / POST requests).
+ * @return {angular.$q.Promise} A promise resolved when the http request has
+ *     a response.
+ */
+yngApp.ApiEndpoint.prototype.saveRequestWithModel = function(action, params, data) {
+    // Copy the given data so that the beforeSave operation doesn't alter the
+    // object state from wherever the request was triggered.
+    var model = angular.copy(data);
+
+    if (model && model.beforeSave) {
+        model.beforeSave();
+    }
+
+    return this.request(action, params, model);
+};
+
+/******************************************************************************/
+
+/**
+ * Angular provider for configuring and instantiating as api service.
+ *
+ * @constructor
+ */
+yngApp.ApiProvider = function() {
+    this.baseRoute = '';
+    this.endpoints = {};
+};
+
+/**
+ * Sets the base server api route.
+ * @param {string} route The base server route.
+ */
+yngApp.ApiProvider.prototype.setBaseRoute = function(route) {
+    this.baseRoute = route;
+};
+
+/**
+ * Creates an api endpoint. The endpoint is returned so that configuration of
+ * the endpoint can be chained.
+ *
+ * @param {string} name The name of the endpoint.
+ * @return {yngApp.ApiEndpointConfig} The endpoint configuration object.
+ */
+yngApp.ApiProvider.prototype.endpoint = function(name) {
+    var endpointConfig = new yngApp.ApiEndpointConfig();
+    this.endpoints[name] = endpointConfig;
+    return endpointConfig;
+};
+
+/**
+ * Function invoked by angular to get the instance of the api service.
+ * @return {Object.<string, yngApp.ApiEndpoint>} The set of all api endpoints.
+ */
+yngApp.ApiProvider.prototype.$get = ['$injector', function($injector) {
+    var api = {};
+
+    var self = this;
+    angular.forEach(this.endpoints, function(endpointConfig, name) {
+        api[name] = $injector.instantiate(yngApp.ApiEndpoint, {
+            baseRoute: self.baseRoute,
+            endpointConfig: endpointConfig
+        });
+    });
+
+    return api;
+}];
+
+
+// Example of creating an angular module to house "core" functionality. It is
+// here that you add any custom providers.
+yngApp.core = angular.module('yng.core', ['ngResource']);
+
+yngApp.core.config(function($provide) {
+    $provide.provider('caas', yngApp.ApiProvider);
+})
+
+;
+
+
+
+
+
