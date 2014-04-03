c0de3aa63b4a3905820140616f3677a06e7bfd6b
Mike Nightingale <mike.nightingale@hybris.com>
2014-04-03 17:09:56 -0600

    added addMore Function

--- public/js/app/products/controllers/browse-products-ctrl.js
+++ public/js/app/products/controllers/browse-products-ctrl.js
@@ -1,7 +1,17 @@
 'use strict';
 
-angular.module('ds.products')
+angular.module('ds.products', ['infinite-scroll'])
    .controller('BrowseProductsCtrl', [ '$scope', 'products', function($scope, products) {
    $scope.products = products;
+
+          $scope.addMore = function(pageSize){
+                products(pageSize, ++$scope.pageNumber).then(
+                    function (products) {
+                        if (products.products){
+                            $scope.products = $scope.products.concat(products.products);
+                        }
+                    }
+                );
+            };
 
 }]);
