60f3aa22fbff351f50271b946c3484e4ef60ad16
Mike Nightingale <mike.nightingale@hybris.com>
2014-04-04 09:34:09 -0600

    added jquery to bwer call working successfully in controller

--- public/js/app/products/controllers/browse-products-ctrl.js
+++ public/js/app/products/controllers/browse-products-ctrl.js
@@ -1,11 +1,12 @@
 'use strict';
 
-angular.module('ds.products', ['infinite-scroll'])
-   .controller('BrowseProductsCtrl', [ '$scope', 'products', function($scope, products) {
+angular.module('ds.products', ['infinite-scroll', 'yng.core'])
+   .controller('BrowseProductsCtrl', [ '$scope', 'products', 'caas', function($scope, products, caas) {
+   
    $scope.products = products;
 
-          $scope.addMore = function(pageSize){
-                products(pageSize, ++$scope.pageNumber).then(
+          $scope.addMore = function(pageSize, pageNumber){
+                caas.products.API.query({pagesize: pageSize, pageNumber: ++$scope.pageNumber}).$promise.then(
                     function (products) {
                         if (products.products){
                             $scope.products = $scope.products.concat(products.products);
