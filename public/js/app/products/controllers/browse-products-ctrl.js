546fde3a0a8d7c35ac917d2a36b42c1409c1eedf
Mike Nightingale <mike.nightingale@hybris.com>
2014-04-04 09:51:30 -0600

    call working properly

--- public/js/app/products/controllers/browse-products-ctrl.js
+++ public/js/app/products/controllers/browse-products-ctrl.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('ds.products', ['infinite-scroll', 'yng.core'])
-   .controller('BrowseProductsCtrl', [ '$scope', 'products', 'caas', function($scope, products, caas) {
+   .controller('BrowseProductsCtrl', [ '$scope', 'products', 'caas', '$state', '$stateParams', function($scope, products, caas, $stateParams) {
    
    $scope.products = products;
+   $scope.pageNumber = ($stateParams.pageNumber || 1);
 
-          $scope.addMore = function(pageSize, pageNumber){
-                caas.products.API.query({pagesize: pageSize, pageNumber: ++$scope.pageNumber}).$promise.then(
+
+          $scope.addMore = function(){
+                caas.products.API.query({pagesize: 15, pageNumber: ++$scope.pageNumber}).$promise.then(
                     function (products) {
                         if (products.products){
                             $scope.products = $scope.products.concat(products.products);
