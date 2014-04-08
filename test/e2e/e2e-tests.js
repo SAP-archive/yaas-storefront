2ea6b7bdc6f2fd0d36da6360733d763580c62391
Mike Nightingale <mike.nightingale@hybris.com>
2014-04-08 15:13:05 -0600

    added product-list test changed initial load of items

--- test/e2e/e2e-tests.js
+++ test/e2e/e2e-tests.js
@@ -8,6 +8,18 @@ describe("product page", function () {
 
         var testProduct = element(by.xpath("//img[contains(@src,'http://img.wonderhowto.com/img/26/99/63418592294146/0/fancy-bike-folds-into-handy-tote-bag.w654.jpg')]"));
 
+        var getTextByRepeaterRow = function findProductByRepeaterRow(number) {
+        	var number
+        	expect(element(by.repeater('product in products').row(number).column('product.name')).getText());
+        }
+
+
+        function scrollToBottomOfProducts(end) {
+        for(y=500; y < end; y+=500) {
+			browser.executeScript('window.scrollTo(800,'+ y + ');');
+			browser.sleep(1000);
+		}     
+        }
 
     it("should display correct title", function () {
           browser.get('#!/products/');
@@ -19,29 +31,16 @@ describe("product page", function () {
       it('should scroll to load more products', function () {
         //test default
         browser.get('#!/products/');
-        browser.sleep(8000);
-
-        expect(element(by.repeater('product in products').row(0).column('product.name')).getText());
-        expect(element(by.repeater('product in products').row(10).column('product.name')).getText());
-        expect(element(by.repeater('product in products').row(15).column('product.name')).getText());
-
-
-		for(y=500; y < 3500; y+=500) {
-			browser.executeScript('window.scrollTo(800,'+ y + ');');
-			browser.sleep(1000);
-		}        
-
-
-
-        expect(element(by.repeater('product in products').row(30).column('product.name')).getText());
+        getTextByRepeaterRow(0)
+        // TODO find out how to append arguments to a function
+        // .toEqual('awesome');
+        getTextByRepeaterRow(10)
+        getTextByRepeaterRow(15)
+        scrollToBottomOfProducts(3500);
+        getTextByRepeaterRow(30)
         testProduct.click();
-        browser.sleep(10000);
-
-
-
-
-
-
+        browser.sleep(5000);
+        
       });
 
   }); 
