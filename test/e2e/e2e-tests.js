a17084e19c79e79616598c5622482c351642e976
Mike Nightingale <mike.nightingale@hybris.com>
2014-04-08 16:06:46 -0600

    added product detail test

--- test/e2e/e2e-tests.js
+++ test/e2e/e2e-tests.js
@@ -4,10 +4,14 @@ describe("product page", function () {
     browser.get('#!/products/');
   });
 
-  describe("verify page", function () {
+  describe("verify product pages", function () {
+
+    beforeEach(function () {
+      browser.get('#!/products');
+    });
 
         var testProduct = element(by.xpath("//img[contains(@src,'http://img.wonderhowto.com/img/26/99/63418592294146/0/fancy-bike-folds-into-handy-tote-bag.w654.jpg')]"));
-
+        var testProductDescription = element(by.binding('product.description'));
         var getTextByRepeaterRow = function findProductByRepeaterRow(number) {
         	var number
         	expect(element(by.repeater('product in products').row(number).column('product.name')).getText());
@@ -15,33 +19,34 @@ describe("product page", function () {
 
 
         function scrollToBottomOfProducts(end) {
-        for(y=500; y < end; y+=500) {
-			browser.executeScript('window.scrollTo(800,'+ y + ');');
-			browser.sleep(1000);
-		}     
+          for(y=500; y < end; y+=500) {
+      			browser.executeScript('window.scrollTo(800,'+ y + ');');
+      			browser.sleep(1000);
+      	   }     
         }
 
-    it("should display correct title", function () {
-          browser.get('#!/products/');
 
-      expect(browser.getTitle()).toBe('Blank Project');
-    });
       
 
       it('should scroll to load more products', function () {
-        //test default
-        browser.get('#!/products/');
+        browser.sleep(10000);
         getTextByRepeaterRow(0)
-        // TODO find out how to append arguments to a function
-        // .toEqual('awesome');
-        getTextByRepeaterRow(10)
-        getTextByRepeaterRow(15)
         scrollToBottomOfProducts(3500);
-        getTextByRepeaterRow(30)
+        getTextByRepeaterRow(15)
         testProduct.click();
         browser.sleep(5000);
         
       });
+
+      it("should get product detail page", function () {
+        browser.sleep(8000);
+        scrollToBottomOfProducts(3500);
+        testProduct.click();
+        browser.sleep(8000);
+
+        expect(testProductDescription.getText()).toEqual('Description:\nA bicycle, often called a bike, is a human-powered, pedal-driven, single-track vehicle, having two wheels attached to a frame, one behind the other. A bicycle rider is called a cyclist, or bicyclist.');
+
+    });
 
   }); 
 }); 
