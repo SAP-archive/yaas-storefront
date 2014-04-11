35f4c01eafb432ae4ff954e25484a3363bb9ee14
Mike O'Donnell <mike.odonnell@hybris.com>
2014-04-11 16:50:41 -0600

    STOR-287 add cart page/route, display items from cart, etc

--- public/js/app/cart/controllers/cart-ctrl.js
+++ public/js/app/cart/controllers/cart-ctrl.js
@@ -13,6 +13,6 @@
 'use strict';
 
 angular.module('ds.cart')
-    .controller('CartCtrl', [ function() {
-
+    .controller('CartCtrl', ['$rootScope', function($rootScope) {
+        var cart = $rootScope.cart;
     }]);
