9162af132adb155a0ec88eaec2fbe58c9158d466
Vera Coberley <vera.coberley@hybris.com>
2014-04-14 16:09:40 -0600

    STOR-297 add scope function validation

--- test/unit/browse-products-ctrl-spec.js
+++ test/unit/browse-products-ctrl-spec.js
@@ -1,30 +1,45 @@
 describe('BrowseProductsCtrl Test', function () {
 
-    var $scope, mockedProductSvc, browseProdCtrl;
+    var $scope, $rootScope, browseProdCtrl, products;
 
-    beforeEach(module(function($provide) {
-        mockedProductSvc = {
-            query: jasmine.createSpy()
-        };
-
-        $provide.value('ProductSvc', mockedProductSvc);
-
-    }));
-
+    // configure the target controller's module for testing - see angular.mock
     beforeEach(angular.mock.module('ds.products'));
 
-    beforeEach(inject(function(_$rootScope_, $controller) {
+    beforeEach(inject(function(_$rootScope_, $controller, $q) {
+
+        this.addMatchers({
+            toEqualData: function (expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
+        $rootScope =  _$rootScope_;
         $scope = _$rootScope_.$new();
+
+        products = [{'name': 'prod1'}];
+
+        // stubbing a service with callback
+        var mockedProductSvc = {
+
+            query: function(parms, callback) {
+                if(callback) {
+                    callback(products);
+                }
+                return products;
+            }
+        };
+
+        // manual injection of the dummy service into the controller
         browseProdCtrl = $controller('BrowseProductsCtrl', {$scope: $scope, 'ProductSvc': mockedProductSvc});
 
     }));
 
-
-    describe('BrowseProductsCtrl', function () {
-        it('should query products on initialization', function () {
-            expect(mockedProductSvc.query).toHaveBeenCalled();
+    describe('BrowseProductsCtrl - addMore', function() {
+        it(' queries products and adds them to the scope', function() {
+            $scope.products = [];
+            $scope.addMore();
+            // validate that "add more" added products returned by query to the scope
+            expect($scope.products).toEqualData(products);
         })
     })
-
 
 });
