4c7151bfd00911589925566616dee2d824e75015
Vera Coberley <vera.coberley@hybris.com>
2014-04-14 16:25:12 -0600

    STOR-297 refactored addMore

--- test/unit/browse-products-ctrl-spec.js
+++ test/unit/browse-products-ctrl-spec.js
@@ -1,11 +1,11 @@
 describe('BrowseProductsCtrl Test', function () {
 
-    var $scope, $rootScope, browseProdCtrl, products;
+    var $scope, $rootScope, $controller;
 
     // configure the target controller's module for testing - see angular.mock
     beforeEach(angular.mock.module('ds.products'));
 
-    beforeEach(inject(function(_$rootScope_, $controller, $q) {
+    beforeEach(inject(function(_$rootScope_, _$controller_, $q) {
 
         this.addMatchers({
             toEqualData: function (expected) {
@@ -14,32 +14,41 @@ describe('BrowseProductsCtrl Test', function () {
         });
         $rootScope =  _$rootScope_;
         $scope = _$rootScope_.$new();
-
-        products = [{'name': 'prod1'}];
-
-        // stubbing a service with callback
-        var mockedProductSvc = {
-
-            query: function(parms, callback) {
-                if(callback) {
-                    callback(products);
-                }
-                return products;
-            }
-        };
-
-        // manual injection of the dummy service into the controller
-        browseProdCtrl = $controller('BrowseProductsCtrl', {$scope: $scope, 'ProductSvc': mockedProductSvc});
-
+        $controller = _$controller_;
     }));
 
-    describe('BrowseProductsCtrl - addMore', function() {
-        it(' queries products and adds them to the scope', function() {
+    describe('BrowseProductsCtrl - addMore', function () {
+
+        var products, browseProdCtrl, stubbedProductSvc;
+
+        beforeEach(function () {
+
+            products = [
+                {'name': 'prod1'}
+            ];
+
+            // stubbing a service with callback
+            stubbedProductSvc = {
+
+                query: function (parms, callback) {
+                    if (callback) {
+                        callback(products);
+                    }
+                    return products;
+                }
+            };
+
+            browseProdCtrl = $controller('BrowseProductsCtrl', {$scope: $scope, 'ProductSvc': stubbedProductSvc})
+        })
+
+        it(' queries products and adds them to the scope', function () {
+
             $scope.products = [];
             $scope.addMore();
             // validate that "add more" added products returned by query to the scope
             expect($scope.products).toEqualData(products);
         })
+
     })
 
 });
