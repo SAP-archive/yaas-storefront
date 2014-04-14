d486efb42f0f4000ed979af3fdcaac54daeeac7f
Vera Coberley <vera.coberley@hybris.com>
2014-04-14 13:35:40 -0600

    STOR-297 first pass at controller unit test

--- public/js/app/products/controllers/browse-products-ctrl.js
+++ public/js/app/products/controllers/browse-products-ctrl.js
@@ -4,8 +4,6 @@ angular.module('ds.products')
     .controller('BrowseProductsCtrl', [ '$scope', 'ProductSvc', function($scope, ProductSvc) {
 
     $scope.pageNumber = ($scope.pageNumber || 1);
-
-
     $scope.products = ProductSvc.query({pageNumber: $scope.pageNumber, pageSize: 5});
 
     $scope.addMore = function(){
--- test/unit/browse-products-ctrl-spec.js
+++ test/unit/browse-products-ctrl-spec.js
@@ -4,36 +4,70 @@
 
 describe('BrowseProductsCtrl Test', function () {
 
-    var $scope, mockedProductSvc, browseProdCtrl;
+    var $scope, q, mockedProductSvc, browseProdCtrl, deferredProducts;
 
-    beforeEach(module('ds.products'));
+    var $rootScope;
+
 
     beforeEach(module(function($provide) {
+       /*
+        mockedProductSvc = {
+            query: function() {
+                deferredProducts = q.defer();
+                deferredProducts.resolve('someValue');
+
+                deferredProducts.$promise = function() {
+                    return deferredProducts.promise;
+                }
+                return deferredProducts.promise;
+            }
+        }; */
         mockedProductSvc = {
             query: jasmine.createSpy()
         };
 
         $provide.value('ProductSvc', mockedProductSvc);
+        $provide.value('ProductSvc', mockedProductSvc);
 
     }));
 
-    beforeEach(module('ds.products.ctrl'));
+    beforeEach(angular.mock.module('ds.products'));
 
-    //beforeEach(module('ds.products'));
+    beforeEach(inject(function(_$rootScope_, $controller, $q){
+            $scope = _$rootScope_.$new();
+            q = $q;
+            deferredProducts = q.defer();
+            deferredProducts.resolve('someValue');
 
-    beforeEach(inject(function(_$rootScope_, $controller) {
-        $scope = _$rootScope_.$new();
-        browseProdCtrl = $controller('BrowseProductsCtrl', {$scope: $scope, 'ProductSvc': mockedProductSvc});
-
+            browseProdCtrl = $controller('BrowseProductsCtrl', {$scope: $scope, 'ProductSvc': mockedProductSvc});
     }));
 
-    /*     NOT WORKING YET
     describe('BrowseProductsCtrl', function() {
-        it('addMore should query product service', function(){
+
+        it('constructor should query product service', function(){
+            /*
+            spyOn(mockedProductSvc, 'query').andReturn(deferredProducts.promise);
+            $scope = _$rootScope_.$new();
+
+            browseProdCtrl = $controller('BrowseProductsCtrl', {$scope: $scope, 'ProductSvc': mockedProductSvc});
+            */
+            expect(mockedProductSvc.query).toHaveBeenCalled();
+            //$scope.addMore();
+            //expect(mockedProductSvc.query).toHaveBeenCalled();
+        })
+
+        /*
+        it('addMore should query product service', inject(function(_$rootScope_, $controller, $q){
+
+            spyOn(mockedProductSvc, 'query');
+            $scope = _$rootScope_.$new();
+            q = $q;
+            browseProdCtrl = $controller('BrowseProductsCtrl', {$scope: $scope, 'ProductSvc': mockedProductSvc});
             $scope.addMore();
             expect(mockedProductSvc.query).toHaveBeenCalled();
-        })
-    }) */
+        })) */
+    })
+
 
 
 
