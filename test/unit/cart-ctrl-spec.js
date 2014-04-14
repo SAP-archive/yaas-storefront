bd54fe81dfdc34cdfdef55b9f821a95a344adfcc
Mike O'Donnell <mike.odonnell@hybris.com>
2014-04-14 13:52:53 -0600

    STOR-287 add cart ctrl unit test

---
+++ test/unit/cart-ctrl-spec.js
@@ -0,0 +1,51 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ */
+
+describe('CartCtrl Test', function () {
+
+    var $scope, mockedCartSvc, cartCtrl;
+
+    beforeEach(module('ds.cart'));
+
+    beforeEach(module(function($provide) {
+        mockedCartSvc = {
+            getCart: function() {
+                return [
+                    {'name': 'Coffee Mug', 'price': 5.00},
+                    {'name': 'Bike', 'price': 100.00}
+                ];
+            }
+        };
+
+        $provide.value('CartSvc', mockedCartSvc);
+
+    }));
+
+    //beforeEach(module('ds.products'));
+
+    beforeEach(inject(function(_$rootScope_, $controller) {
+        $scope = _$rootScope_.$new();
+        cartCtrl = $controller('CartCtrl', {$scope: $scope, 'CartSvc': mockedCartSvc});
+
+        this.addMatchers({
+            toEqualData: function (expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
+
+    }));
+
+    it('Should calculate subtotal', function() {
+        expect($scope.subtotal).toEqualData(105.00);
+    });
+
+});
