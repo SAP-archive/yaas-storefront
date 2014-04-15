29e5e647bb7b23c891ccb234cb1453e0bad52d9c
Vera Coberley <vera.coberley@hybris.com>
2014-04-15 14:10:19 -0600

    prod service refactoring; attempt at svc test

--- public/js/app/products/services/product-service.js
+++ public/js/app/products/services/product-service.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ *  Encapsulates access to the CAAS product API.
+ */
 angular.module('ds.products')
     .factory('ProductSvc', ['caas', function(caas){
 
@@ -8,16 +11,26 @@ angular.module('ds.products')
         };
 
         return {
-            query: function(parms, callback) {
-                // no callback utilize return of array that will be filled once data is loaded
-                if(!callback) {
-                    return getProducts(parms);
-                } else {
-                    // with callback - invoke callback method once promise is filled
-                    getProducts(parms).$promise.then(function (result) {
-                        callback(result);
-                    });
-                }
+            /**
+             * Issues a query request on the product resource.
+             * @param {parms} query parameters - optional
+             * @return The result array as returned by Angular $resource.query().
+             */
+            query: function(parms) {
+               return getProducts(parms);
+            },
+
+            /**
+             * Registers a success callback handler on the API 'query' request - invoked once the
+             * promise is resolved.
+             * @param {parms} query parameters
+             * @param {callback} success callback function
+             */
+            queryWithResultHandler: function(parms, callback) {
+                getProducts(parms).$promise.then(function (result) {
+                    callback(result);
+                });
             }
+
         };
 }]);
---
+++ test/unit/product-svc-spec.js
@@ -0,0 +1,86 @@
+/**
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ */
+describe('ProductSvc Test', function () {
+
+    var productUrl = 'http://product-service-dprod.deis-dev-01.ytech.fra.hybris.com';
+    var productRoute = 'products' ;
+    var $scope, $rootScope, $httpBackend, productSvc;
+
+
+    //beforeEach(angular.module('yng.core'));
+
+    //beforeEach(angular.module('ds.products'));
+    /*
+    , function(caasProvider) {
+
+        caasProvider.setBaseRoute(productUrl);
+        caasProvider.endpoint('products', { productSku: '@productSku' }).
+            route(productRoute);
+    }));    */
+
+
+    beforeEach(function () {
+        this.addMatchers({
+            toEqualData: function (expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
+
+        inject(function(_$httpBackend_, _$rootScope_) {
+            $rootScope = _$rootScope_;
+            $scope = _$rootScope_.$new();
+
+            $httpBackend  = _$httpBackend_;
+
+        });
+    });
+
+
+
+
+
+        //var productSvc;
+
+
+
+        //beforeEach(inject(function(ProductSvc){
+          //  productSvc = ProductSvc;
+        //}));
+
+        it('query returns product array', function () {
+            expect('test').toBeTruthy();
+            /*
+            $httpBackend.expectGET('http://product-service-dprod.deis-dev-01.ytech.fra.hybris.com/products').respond([
+                {name: 'Shirt'},
+                {name: 'Hat'}
+            ]);
+
+            var products = productSvc.query();
+
+            expect(products).toBeUndefined();
+
+            $httpBackend.flush();
+
+            expect(products).toEqualData({"products": [
+                {name: 'Shirt'},
+                {name: 'Hat'}
+            ]});
+            */
+        })
+
+        /*
+         it('query with callback invokes it on resolved promise', function () {
+
+         })   */
+
+
+});
