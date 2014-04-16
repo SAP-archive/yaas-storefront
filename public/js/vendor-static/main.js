323d7fc231647156b66d0714b7053544d30f3795
Mike O'Donnell <mike.odonnell@hybris.com>
2014-04-16 11:45:44 -0600

    STOR-286 update main.js

--- public/js/vendor-static/main.js
+++ public/js/vendor-static/main.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
 	$('[data-toggle=offcanvas]').click(function () {
-		$('.off-canvas').toggleClass('active');
+		$('.off-canvas').toggleClass('left');
+	});
+
+	$('[data-toggle=offcanvas-cart]').click(function () {
+		$('.off-canvas').toggleClass('right');
 	});
 
 
 
-    /*
+
+
 
 	$(".gridMasonry").imagesLoaded( function() {
 			$(".gridMasonry").masonry({
 				itemSelector: '.item',
 				isInitLayout: true
 		});
-	});  */
+	});
 
 
  	$(document).on("click",".showRefineContainer",function(e){
@@ -75,7 +80,7 @@ $(document).ready(function () {
 		});
 		$(".gallery .thumbnails").owlCarousel({
 			items : 5,
-			itemsCustom:[[0, 2], [400, 4], [700, 4], [1000, 4], [1200, 5], [1600, 16]],
+			itemsCustom:[[0, 2], [400, 4], [700, 4], [1000, 4], [1200, 5], [1600, 10]],
 			navigation:true,
 			navigationText:false,
 			lazyLoad : true,
@@ -130,7 +135,7 @@ $(document).ready(function () {
 		 	e.preventDefault();
 
 
-		 	if(!isTap){
+		 	if(isTap){
 		 		initZoom("tap")
 
 		 	}else{
