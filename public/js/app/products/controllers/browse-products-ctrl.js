2d1fc46eca8faafda5f60c2012e7e0b0b8f02d55
Mike O'Donnell <mike.odonnell@hybris.com>
2014-04-21 14:34:59 -0600

    back to top button added

--- public/js/app/products/controllers/browse-products-ctrl.js
+++ public/js/app/products/controllers/browse-products-ctrl.js
@@ -29,6 +29,10 @@ angular.module('ds.products')
             return ProductSvc.query({pageNumber: $scope.pageNumber, pageSize: $scope.pageSize, sort: $scope.sort});
         };
 
+        $scope.backToTop = function () {
+            window.scrollTo(0, 0);
+        };
+
 
         $scope.setSortedPage = function (pageNo) {
             $scope.pageNumber = pageNo;
