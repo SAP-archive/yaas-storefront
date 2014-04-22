9e7fd612fd6722e5414755390196a114e837c408
Mike Nightingale <mike.nightingale@hybris.com>
2014-04-22 09:08:54 -0600

    commented out cart tests. Removed JS files from product-list

--- test/e2e/cart-tests.js
+++ test/e2e/cart-tests.js
@@ -1,105 +1,105 @@
-    describe("cart:", function () {
+//     describe("cart:", function () {
 
-  beforeEach(function () {
-    browser.get('#!/products');
-  });
+//   beforeEach(function () {
+//     browser.get('#!/products');
+//   });
 
-  describe("verify cart functionality", function () {
+//   describe("verify cart functionality", function () {
 
-    beforeEach(function () {
-      browser.get('#!/products');
-      browser.sleep(8000);
-    });
+//     beforeEach(function () {
+//       browser.get('#!/products');
+//       browser.sleep(8000);
+//     });
 
-        var testProduct1 = "//img[contains(@src,'http://img.wonderhowto.com/img/26/99/63418592294146/0/fancy-bike-folds-into-handy-tote-bag.w654.jpg')]";
-        var testProduct2 = "//img[contains(@src,'http://sand-bsd-2.yrdrt.fra.hybris.com/Products/Chemex.jpg')]";
-        var testProduct3 = "//img[contains(@src,'http://sand-bsd-2.yrdrt.fra.hybris.com/Products/image_16.jpg')]";
-        var cartButton = "//button[@type='button']";
-        var buyButton = "//div[2]/div/button";
-        var contineShopping = "//div[@id='cart']/div/div/button";
-        var removeFromCart = "//div[@id='cart']/section[2]/div/div/div[2]/button"
-        var cartButton2 = "div.off-canvas > div.ng-scope > nav.full-nav.ng-scope > button.btn.btn-link.navbar-btn.pull-right.cart"
+//         var testProduct1 = "//img[contains(@src,'http://img.wonderhowto.com/img/26/99/63418592294146/0/fancy-bike-folds-into-handy-tote-bag.w654.jpg')]";
+//         var testProduct2 = "//img[contains(@src,'http://sand-bsd-2.yrdrt.fra.hybris.com/Products/Chemex.jpg')]";
+//         var testProduct3 = "//img[contains(@src,'http://sand-bsd-2.yrdrt.fra.hybris.com/Products/image_16.jpg')]";
+//         var cartButton = "//button[@type='button']";
+//         var buyButton = "//div[2]/div/button";
+//         var contineShopping = "//div[@id='cart']/div/div/button";
+//         var removeFromCart = "//div[@id='cart']/section[2]/div/div/div[2]/button"
+//         var cartButton2 = "div.off-canvas > div.ng-scope > nav.full-nav.ng-scope > button.btn.btn-link.navbar-btn.pull-right.cart"
 
-        function verifyCartAmount(amount) {
-          expect(element(by.xpath("//input[@type='number']")).getAttribute("value")).toEqual(amount);
-        }
+//         function verifyCartAmount(amount) {
+//           expect(element(by.xpath("//input[@type='number']")).getAttribute("value")).toEqual(amount);
+//         }
 
-        function verifyCartTotal(total) {
-          expect(element(by.css("td.text-right.ng-binding")).getText()).toEqual(total);
-        }
+//         function verifyCartTotal(total) {
+//           expect(element(by.css("td.text-right.ng-binding")).getText()).toEqual(total);
+//         }
 
-        function clickButtonByXpath(path) {
-          element(by.xpath(path)).click();
-        }
+//         function clickButtonByXpath(path) {
+//           element(by.xpath(path)).click();
+//         }
 
-        function clickByCss(link) {  
-          element(by.css(link)).click();
-        }
+//         function clickByCss(link) {  
+//           element(by.css(link)).click();
+//         }
 
 
 
-      it('should load one product into cart', function () {
-        // element(by.css(cartButton2)).click()
-        clickButtonByXpath(cartButton);
-        browser.sleep(500);
-        verifyCartTotal("$0.00");
-        clickButtonByXpath(contineShopping);
-        clickButtonByXpath(testProduct1);
-        clickButtonByXpath(buyButton);
-        browser.sleep(500);
-        verifyCartAmount("1");
-        verifyCartTotal("$9.50");
-        clickButtonByXpath(removeFromCart);
-        verifyCartTotal("$0.00");
-      });
+//       it('should load one product into cart', function () {
+//         // element(by.css(cartButton2)).click()
+//         clickButtonByXpath(cartButton);
+//         browser.sleep(500);
+//         verifyCartTotal("$0.00");
+//         clickButtonByXpath(contineShopping);
+//         clickButtonByXpath(testProduct1);
+//         clickButtonByXpath(buyButton);
+//         browser.sleep(500);
+//         verifyCartAmount("1");
+//         verifyCartTotal("$9.50");
+//         clickButtonByXpath(removeFromCart);
+//         verifyCartTotal("$0.00");
+//       });
 
-        it('should load multiple products into cart', function () {
-          clickButtonByXpath(cartButton);
-          verifyCartTotal("$0.00");
-          clickButtonByXpath(contineShopping);  
-          clickButtonByXpath(testProduct1);
-          clickButtonByXpath(buyButton);
-          browser.sleep(500);
-          verifyCartAmount("1");
-          verifyCartTotal("$9.50");
-          clickButtonByXpath(contineShopping);
-          clickByCss("img");
-          clickButtonByXpath(testProduct3);
-          clickButtonByXpath(buyButton);
-          verifyCartTotal("$11.50");
+//         it('should load multiple products into cart', function () {
+//           clickButtonByXpath(cartButton);
+//           verifyCartTotal("$0.00");
+//           clickButtonByXpath(contineShopping);  
+//           clickButtonByXpath(testProduct1);
+//           clickButtonByXpath(buyButton);
+//           browser.sleep(500);
+//           verifyCartAmount("1");
+//           verifyCartTotal("$9.50");
+//           clickButtonByXpath(contineShopping);
+//           clickByCss("img");
+//           clickButtonByXpath(testProduct3);
+//           clickButtonByXpath(buyButton);
+//           verifyCartTotal("$11.50");
 
-        });
+//         });
 
-        it('should update quantity', function () {
-          clickButtonByXpath(cartButton);
-          verifyCartTotal("$0.00");
-          clickButtonByXpath(contineShopping);
-          clickButtonByXpath(testProduct1);
-          clickButtonByXpath(buyButton);
-          browser.sleep(500);
-          verifyCartAmount("1");
-          verifyCartTotal("$9.50");
-          clickButtonByXpath(contineShopping);
-          clickButtonByXpath(buyButton);
-          verifyCartAmount("2");
-          verifyCartTotal("$19.00");
-          element(by.xpath("//input[@type='number']")).clear();
-          element(by.xpath("//input[@type='number']")).sendKeys("5");
-          verifyCartAmount("5");
-          verifyCartTotal("$47.50");
-        });
+//         it('should update quantity', function () {
+//           clickButtonByXpath(cartButton);
+//           verifyCartTotal("$0.00");
+//           clickButtonByXpath(contineShopping);
+//           clickButtonByXpath(testProduct1);
+//           clickButtonByXpath(buyButton);
+//           browser.sleep(500);
+//           verifyCartAmount("1");
+//           verifyCartTotal("$9.50");
+//           clickButtonByXpath(contineShopping);
+//           clickButtonByXpath(buyButton);
+//           verifyCartAmount("2");
+//           verifyCartTotal("$19.00");
+//           element(by.xpath("//input[@type='number']")).clear();
+//           element(by.xpath("//input[@type='number']")).sendKeys("5");
+//           verifyCartAmount("5");
+//           verifyCartTotal("$47.50");
+//         });
 
-        it('should not add out of stock item', function () {
-          clickButtonByXpath(cartButton);
-          verifyCartTotal("$0.00");
-          clickButtonByXpath(contineShopping);
-          clickButtonByXpath(testProduct2);
-          clickButtonByXpath("//div[2]/button"); //out of stock button
-          clickButtonByXpath(cartButton);
-          verifyCartTotal("$0.00");
-          clickButtonByXpath(contineShopping);
-        });
+//         it('should not add out of stock item', function () {
+//           clickButtonByXpath(cartButton);
+//           verifyCartTotal("$0.00");
+//           clickButtonByXpath(contineShopping);
+//           clickButtonByXpath(testProduct2);
+//           clickButtonByXpath("//div[2]/button"); //out of stock button
+//           clickButtonByXpath(cartButton);
+//           verifyCartTotal("$0.00");
+//           clickButtonByXpath(contineShopping);
+//         });
 
-  }); 
-}); 
+//   }); 
+// }); 
 
