8006a001d977e9beb5572811c3e1ce2904185c70
Vera Coberley <vera.coberley@hybris.com>
2014-04-25 15:06:23 -0600

    STOR-290 remove unnecessary this/self rename

--- public/js/app/checkout/services/order-service.js
+++ public/js/app/checkout/services/order-service.js
@@ -31,14 +31,12 @@ angular.module('ds.checkout')
              * @return The result array as returned by Angular $resource.query().
              */
             createOrder: function(cartItems) {
-                var self = this;
 
                 var OrderLine = function(amount, unitPrice, productCode) {
                     this.amount = amount;
                     this.unitPrice = unitPrice;
                     this.productCode = productCode;
                 };
-
 
                 var Order = function(){
                     this.entries = [];
@@ -51,7 +49,7 @@ angular.module('ds.checkout')
                 });
 
                 caas.orders.API.save(newOrder).$promise.then(function(order){
-                    self.setLastOrderId(order.id);
+                    this.setLastOrderId(order.id);
                 });
             }
         };
