4eeb92569c3756f49037fbe7d01012d78980cbff
Vera Coberley <vera.coberley@hybris.com>
2014-04-28 12:33:21 -0600

    STOR-290 add checkout-ctrl unit tests

--- test/unit/checkout/checkout-ctrl-spec.js
+++ test/unit/checkout/checkout-ctrl-spec.js
@@ -1,6 +1,7 @@
 describe('CheckoutCtrl Test', function () {
 
-    var $scope, $rootScope, $controller, $injector;
+    var $scope, $rootScope, $controller, $injector, mockedOrderSvc, mockedCartSvc, checkoutCtrl;
+    var mockBillTo = {'firstName': 'Bob', 'lastName':'Sushi'};
 
     //***********************************************************************
     // Common Setup
@@ -23,7 +24,101 @@ describe('CheckoutCtrl Test', function () {
         $injector = _$injector_;
     }));
 
-    describe('CheckoutCtrl', function () {
+    beforeEach(function () {
+        mockedOrderSvc = $injector.get( 'OrderSvc' );
+        mockedOrderSvc.createOrder = jasmine.createSpy('createOrder');
+        mockedCartSvc = $injector.get('CartSvc');
+        mockedCartSvc.getCart = jasmine.createSpy('getCart');
+        checkoutCtrl = $controller('CheckoutCtrl', {$scope: $scope, 'OrderSvc': mockedOrderSvc, 'CartSvc': mockedCartSvc});
+    });
+
+    describe('Initialization', function () {
+        it('should initialize', function () {
+            expect(checkoutCtrl).toBeTruthy();
+            expect($scope.order).toBeTruthy();
+            expect($scope.wiz).toBeTruthy();
+        })
+    });
+
+
+
+
+    describe('Wizard in Mobile - step completion', function () {
+        beforeEach(function () {
+            $scope.wiz.step1Done = false;
+            $scope.wiz.step2Done = false;
+            $scope.wiz.step3Done = false;
+        });
+
+        it('should set Step 1 Done when bill to entered', function(){
+            $scope.wiz.step1Done = false;
+            $scope.billToDone();
+            expect($scope.wiz.step1Done).toEqualData(true);
+            expect($scope.wiz.step2Done).toEqualData(false);
+            expect($scope.wiz.step2Done).toEqualData(false);
+        });
+
+        it('should set Step 2 Done when ship to to entered', function(){
+            $scope.wiz.step2Done = false;
+            $scope.shipToDone();
+            expect($scope.wiz.step2Done).toEqualData(true);
+            expect($scope.wiz.step3Done).toEqualData(false);
+        });
+
+        it('should set Step 3 Done when shipping entered', function(){
+            $scope.wiz.step3Done = false;
+            $scope.paymentDone();
+            expect($scope.wiz.step3Done).toEqualData(true);
+        });
+
+
+
+    });
+
+    describe('Wizard in Mobile - Editing Complete Information', function () {
+        beforeEach(function () {
+            $scope.wiz.step1Done = true;
+            $scope.wiz.step2Done = true;
+            $scope.wiz.step3Done = true;
+        });
+
+        it(' (Bill To) should set Steps 1,2, 3 undone', function () {
+
+            $scope.editBillTo();
+            expect($scope.wiz.step1Done).toEqualData(false);
+            expect($scope.wiz.step2Done).toEqualData(false);
+            expect($scope.wiz.step3Done).toEqualData(false);
+        });
+
+        it(' (Ship To) should set Steps 2, 3 undone', function () {
+
+            $scope.editShipTo();
+            expect($scope.wiz.step1Done).toEqualData(true);
+            expect($scope.wiz.step2Done).toEqualData(false);
+            expect($scope.wiz.step3Done).toEqualData(false);
+        });
+
+        it(' (Shipping) should set Steps 3 undone', function () {
+            $scope.editPayment();
+            expect($scope.wiz.step1Done).toEqualData(true);
+            expect($scope.wiz.step2Done).toEqualData(true);
+            expect($scope.wiz.step3Done).toEqualData(false);
+        });
+
+    });
+
+    describe('setShipToSameAsBillTo ', function () {
+
+        it('should copy billing to shipping', function(){
+            $scope.order.billTo = mockBillTo;
+            $scope.setShipToSameAsBillTo();
+            expect($scope.order.shipTo).toEqualData(mockBillTo);
+        });
+
+
+    });
+
+    describe('Place Order ', function () {
         var mockedOrderSvc, mockedCartSvc, checkoutCtrl;
 
         beforeEach(function () {
@@ -42,6 +137,13 @@ describe('CheckoutCtrl Test', function () {
             $scope.placeOrder();
             expect(mockedOrderSvc.createOrder).toHaveBeenCalled();
             expect(mockedCartSvc.getCart).toHaveBeenCalled();
+        });
+
+        it('should ensure ship to copy', function(){
+            $scope.order.billTo = mockBillTo;
+            $scope.wiz.shipToSameAsBillTo = true;
+            $scope.placeOrder();
+            expect($scope.order.shipTo).toEqualData(mockBillTo);
         });
     });
 
