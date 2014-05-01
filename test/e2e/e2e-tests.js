f236e0c1e7cbeb47e700138ad26030649335d9ed
Mike Nightingale <mike.nightingale@hybris.com>
2014-05-01 10:59:52 -0600

    added tests for product count [STOR-365]

--- test/e2e/e2e-tests.js
+++ test/e2e/e2e-tests.js
@@ -18,13 +18,20 @@ describe("product page", function () {
         tu.getTextByRepeaterRow(35) //verify last product has loaded
         tu.clickElement('xpath', tu.backToTopButton);        
         tu.clickElement('xpath', tu.bicycle);
-        
+      });
+
+      it('should show the user how many products loaded', function () {
+        tu.getTextByRepeaterRow(0)
+        expect(element(by.css('div.page-indicator.ng-binding')).getText()).toEqual('1-12 of 0'); //should be # of 36, but won't work in phantomjs
+        tu.scrollToBottomOfProducts(10000);
+        tu.getTextByRepeaterRow(35) //verify last product has loaded
+        expect(element(by.css('div.col-xs-6 > div.viewingContainer > div.page-indicator.ng-binding')).getText()).toEqual('1-36 of 0'); //should be # of 36, but won't work in phantomjs
       });
 
       it("should get product detail page", function () {
         tu.scrollToBottomOfProducts(3500);
         tu.clickElement('xpath', tu.bicycle);
-        browser.sleep(8000);
+        browser.sleep(100);
         expect(tu.bicycleDescription.getText()).toEqual('Description:\nA bicycle, often called a bike, is a human-powered, pedal-driven, single-track vehicle, having two wheels attached to a frame, one behind the other. A bicycle rider is called a cyclist, or bicyclist.');
 
     });
