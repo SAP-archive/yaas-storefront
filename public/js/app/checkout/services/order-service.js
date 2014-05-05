fd2bb565dcd22d987ced35271c809186a25ae37f
Mike O'Donnell <mike.odonnell@hybris.com>
2014-05-05 16:39:57 -0600

    order object needs to contain customer, billing, and shipping references

--- public/js/app/checkout/services/order-service.js
+++ public/js/app/checkout/services/order-service.js
@@ -39,6 +39,9 @@ angular.module('ds.checkout')
 
                 var Order = function(){
                     this.entries = [];
+                    this.customer = 'customer account reference';
+                    this.billingAddress = 'billing Address reference';
+                    this.shippingAddress = 'shipping Address reference';
                 };
 
                 var newOrder = new Order();
