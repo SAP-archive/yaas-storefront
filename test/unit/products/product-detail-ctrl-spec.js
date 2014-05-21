a3a5b82a403f956df436850ec9005c49de4ebc98
Mike O'Donnell <mike.odonnell@hybris.com>
2014-05-21 17:11:26 -0600

    add product-detail-ctrl unit test

---
+++ test/unit/products/product-detail-ctrl-spec.js
@@ -0,0 +1,66 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ */
+
+describe('ProductDetailCtrl Test', function () {
+
+    var $scope, $rootScope, $controller;
+
+    //***********************************************************************
+    // Common Setup
+    // - shared setup between constructor validation and method validation
+    //***********************************************************************
+
+    // configure the target controller's module for testing - see angular.mock
+    beforeEach(angular.mock.module('ds.products'));
+
+    beforeEach(inject(function(_$rootScope_, _$controller_) {
+
+        this.addMatchers({
+            toEqualData: function (expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
+        $rootScope =  _$rootScope_;
+        $scope = _$rootScope_.$new();
+        $controller = _$controller_;
+    }));
+
+    describe('ProductDetailCtrl ', function () {
+        var mockedCartSvc, productDetailCtrl;
+
+        beforeEach(function () {
+
+            var mockProduct = {
+                name: 'product1',
+                price: '5000'
+            };
+
+            // creating the mocked service
+            mockedCartSvc = {
+                addProductToCart: jasmine.createSpy()
+            };
+
+            productDetailCtrl = $controller('ProductDetailCtrl', {$scope: $scope, $rootScope: $rootScope,
+                'CartSvc': mockedCartSvc, 'product': mockProduct});
+        });
+
+
+        it('should add to cart from detail page', function(){
+
+            $scope.addToCartFromDetailPage();
+            expect(mockedCartSvc.addProductToCart).toHaveBeenCalled();
+
+        });
+
+    });
+
+});
