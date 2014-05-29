b025145e3c691b27d5cc469c382bfbc5a012600b
Mike O'Donnell <mike.odonnell@hybris.com>
2014-05-29 15:01:53 -0600

    STOR-338 fix checkout tenant

--- public/js/app/router.js
+++ public/js/app/router.js
@@ -196,7 +196,7 @@ window.app = angular.module('ds.router', [
                     }
 
                     else if(config.url.indexOf('checkout')>-1) {
-                        config.headers[settings.apis.headers.tenantOld] = settings.tenantId;
+                        config.headers[settings.apis.headers.tenant] = settings.tenantId;
                     }
 
                     else if(config.url.indexOf('orders')>-1) {
