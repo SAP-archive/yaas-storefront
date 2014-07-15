c9a26b63d7a7f6e95052af8746570865d9590954
Mike O'Donnell <mike.odonnell@hybris.com>
2014-07-15 16:45:18 -0600

    STOR-727 add english translations for demo store, still need to do it for confirmation page

--- public/js/app/shared/i18n/lang/en.js
+++ public/js/app/shared/i18n/lang/en.js
@@ -3,6 +3,79 @@
 angular.module('ds.i18n')
 	.constant('TranslationsEN', {
 
+        /*
+            cart
+         */
+        CHECKOUT: 'Checkout',
+        CONTINUE_SHOPPING: 'Continue shopping',
+        EST_ORDER_TOTAL: 'Est. order total',
+        FREE_SHIPPING: 'Free Shipping and Returns in the US',
+        ITEM: 'Item',
+        ITEM_PRICE: 'Item Price',
+        TOTAL_PRICE: 'Total Price',
+
+        /*
+            checkout
+         */
+        ADDRESS: 'Address',
+        BILLING_ADDRESS: 'Billing Address',
+        CITY: 'City',
+        CONTINUE: 'Continue',
+        COUNTRY: 'Country',
+        CREDIT_CARD_NUMBER: 'Credit Card Number',
+        CVC_NUMBER: 'CVC Number',
+        DELIVERY_METHOD: 'Delivery Method',
+        EMAIL: 'Email',
+        FIRST_NAME: 'First Name',
+        FREE_STANDARD_SHIPPING: 'Free Standard Shipping',
+        LAST_NAME: 'Last Name',
+        METHOD: 'Method',
+        MONTH: 'Month',
+        OPTIONAL: 'Optional',
+        ORDER_DETAILS: 'Order Details',
+        ORDER_TOTAL: 'Order Total',
+        PAYMENT: 'Payment',
+        PLACE_ORDER: 'Place Order',
+        PLEASE_ENTER_VALID_CC: 'Please enter a valid credit card number',
+        PLEASE_ENTER_VALID_CODE: 'Please enter a valid code',
+        PLEASE_ENTER_VALID_EMAIL: 'Please enter a valid email in the format name@example.com',
+        SAME_AS_BILLING_ADDRESS: 'Same As Billing Address',
+        SECURE_CHECKOUT: 'Secure Checkout',
+        SHIPPING: 'Shipping',
+        SHIPPING_ADDRESS: 'Shipping Address',
+        SIMPLE_3_STEP_CHECKOUT: 'Simple 3 Step Checkout',
+        SIMPLE_4_STEP_CHECKOUT: 'Simple 4 Step Checkout',
+        STATE: 'State',
+        STEP_1_MY_DETAILS: 'Step 1. My Details',
+        STEP_2_SHIPPING_INFORMATION: 'Step 2. Shipping Information',
+        STEP_3_PAYMENT: 'Step 3. Payment',
+        STEP_4_REVIEW_AND_PAYMENT: 'Step 4. Review and Payment',
+        SUBTOTAL: 'Subtotal',
+        TAX: 'Tax',
+        TOTAL: 'Total',
+        YEAR: 'Year',
+        ZIP: 'Zip Code',
+
+        /*
+            navigation
+         */
+        PRODUCTS: 'Products',
+
+        /*
+            product detail page
+         */
+        BUY: 'Buy',
+        DESCRIPTION: 'Description',
+        QTY: 'Qty',
+
+        /*
+            product list page
+         */
+        OF: 'of',
+        SORT_BY: 'Sort by',
+        VIEWING: 'Viewing',
+
+
 		EMPTY_MSG: 'Demo Store - Coming Soon',
 		LANGUAGES: 'Languages',
 		en: 'English',
