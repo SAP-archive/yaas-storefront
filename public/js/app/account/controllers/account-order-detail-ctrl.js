a797a5149c8b963169e781567fb8c4a136071fb4
O'Donnell <mike.odonnell@hybris.com>
2014-10-06 12:12:15 -0600

    STOR-995 order detail page now using updated order svc

--- public/js/app/account/controllers/account-order-detail-ctrl.js
+++ public/js/app/account/controllers/account-order-detail-ctrl.js
@@ -13,44 +13,11 @@
 'use strict';
 
 angular.module('ds.account')
-    .controller('AccountOrderDetailCtrl', ['$scope', 'order', 'ProductSvc', 'PriceSvc', '$stateParams', 'GlobalData', function($scope, order, ProductSvc, PriceSvc, $stateParams, GlobalData) {
+    .controller('AccountOrderDetailCtrl', ['$scope', 'order', '$stateParams', 'GlobalData', function($scope, order, $stateParams, GlobalData) {
 
         $scope.order = order;
         $scope.order.id = $stateParams.orderId;
-        $scope.orderProducts = [];
-        $scope.prices = {};
         $scope.currencySymbol = GlobalData.getCurrencySymbol();
-
-        /*
-         Retrieves pricing information for the list of products.
-         */
-        var getPrices = function() {
-            var productIds = '';
-            angular.forEach($scope.order.entries, function (entry, key) {
-                if (key === $scope.order.entries.length - 1) {
-                    productIds = productIds + (entry.product.id);
-                }
-                else {
-                    productIds = productIds + (entry.product.id + ',');
-                }
-            });
-            var queryPrices = {
-                q: 'productId:(' + productIds + ')' + ' currency:' + GlobalData.storeCurrency
-            };
-
-            PriceSvc.query(queryPrices).then(
-                function (pricesResponse) {
-                    if (pricesResponse) {
-                        var pricesMap = {};
-
-                        pricesResponse.forEach(function (price) {
-                            pricesMap[price.productId] = price;
-                        });
-
-                        $scope.prices = angular.extend($scope.prices, pricesMap);
-                    }
-                });
-        };
 
         var getPaymentInfo = function () {
             var payment = {};
@@ -84,44 +51,8 @@ angular.module('ds.account')
             return count;
         };
 
-        /*
-         this function is necessary because the order entries array does not contain
-         all the product information we need
-         */
-        var getProductsInOrder = function () {
-            var query = {};
-            var ids = '';
-
-            angular.forEach(order.entries, function (entry, key) {
-                ids += entry.product.id;
-                if (key !== ($scope.order.entries.length - 1)) {
-                    ids += ',';
-                }
-            });
-
-            query.q = 'id:(' + ids + ')';
-
-            return query;
-
-        };
-
         $scope.itemCount = getItemsOrderedCount();
 
         $scope.payment = getPaymentInfo();
-
-        var query = getProductsInOrder();
-
-        getPrices();
-
-        ProductSvc.query(query).then(function (products) {
-            if (products) {
-                $scope.orderProducts = products;
-                angular.forEach(order.entries, function (entry, key) {
-                    if ($scope.orderProducts.length > key) {
-                        $scope.orderProducts[key].quantity = entry.amount;
-                    }
-                });
-            }
-        });
 
     }]);
