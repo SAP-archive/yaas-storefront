1230ab2ae58dd94302734d7da15649c265d98979
Jeremy Rawls <jeremy.rawls@hybris.com>
2014-12-10 11:25:42 -0600

    updating images to yaas specific

--- public/js/app/home/controllers/home-ctrl.js
+++ public/js/app/home/controllers/home-ctrl.js
@@ -17,23 +17,16 @@ angular.module('ds.home')
         function ($scope) {
 
             $scope.carouselInterval = 5000;
-            var slides = $scope.slides = [];
-
-
-            // ????? UPDATES NEEDED BELOW ????? //
-            // below is only to create place holder content.
-            //$scope.slides should be populated by a service that has real inamgs
-            $scope.addSlide = function() {
-                var newWidth = 600 + slides.length + 1;
-                slides.push({
-                    image: 'http://placekitten.com/' + newWidth + '/300',
-                    text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-                        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-                });
-            };
-            for (var i=0; i<4; i++) {
-                $scope.addSlide();
-            }
+            $scope.slides = [
+                {
+                    image: './img/homePg-hero-audio.jpg',
+                    url: 'http://www.google.com'
+                },
+                {
+                    image: './img/homePg-hero-office.jpg',
+                    url: 'http://www.google.com'
+                }
+            ];
 
 
         }]
