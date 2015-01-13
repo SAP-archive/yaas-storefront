3bf5918052178bd22945d9a803fb8700bf8d0776
Clint Ashworth <clintonnash44@gmail.com>
2015-01-13 15:53:59 -0700

    STOR-1338: clean up

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -16,7 +16,7 @@
 (function () {
 
     var storeConfig = {};
-    storeConfig.storeTenant = '8bwhetym79cq';
+    storeConfig.storeTenant = 'defaultproj';
 
     try {
         var pathLength = window.location.pathname.length;
