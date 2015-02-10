0dff7d37075cbcb729f2435b46f898c8286a885a
O'Donnell <mike.odonnell@hybris.com>
2015-02-10 16:12:00 -0700

    STOR-1443 move mobile wizard to directive

--- public/js/app/checkout/controllers/checkout-ctrl.js
+++ public/js/app/checkout/controllers/checkout-ctrl.js
@@ -19,18 +19,10 @@ angular.module('ds.checkout')
  * The scope provides access to the data models "order" and "cart", as well as some properties to control display
  * of errors.
  *
- * This controller includes a checkout wizard for the mobile checkout process, which requires that information
- * is filled out in segments ("steps"), and all subsequent steps are blocked from input until the required
- * information has been provided for previous steps. If the user edits a previously completed step, all subsequent steps
- * are marked "undone" again, and the user has to tab through the remaining steps to advance.  Validation of
- * required fields comes into play as the user attempts to advance to the next.  If there are missing fields,
- * the missing fields will be highlighted as errors, and the user cannot advance until the necessary information
- * has been provided.
- *
  * In the checkout HTML, the "steps" are created by using nested forms which can be individually validated.
  *
- * The wizard does not come into play in full screen mode.  Required fields are checked and enforced when the user
- * indicates "submit".
+ * The wizard directive defined in mobileCheckoutWizard does not come into play in full screen mode.  Required fields
+ * are checked and enforced when the user indicates "submit".
  *
  * The controller also includes logic to copy the bill-to address to the ship-to address if that's what the user has indicated.
  *
@@ -145,85 +137,6 @@ angular.module('ds.checkout')
                         this.instance.dismiss('cancel');
                     }
                 };
-
-
-
-            var Wiz = function () {
-                this.step1Done = false;
-                this.step2Done = false;
-                this.step3Done = false;
-                this.shipToSameAsBillTo = true;
-                // credit card expiration year drop-down - go 10 years out
-                this.years = [];
-                for (var year = new Date().getFullYear(), i = year, stop = year + 10; i < stop; i++) {
-                    this.years.push(i);
-                }
-                this.months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
-
-            };
-
-            $scope.wiz = new Wiz();
-
-            /** Mark mobile wizard step 1 "done" - bill-to address information has been entered.*/
-            $scope.billToDone = function (billToFormValid, form) {
-                $scope.$broadcast('submitting:form', form);
-                if (billToFormValid) {
-                    $scope.wiz.step1Done = true;
-                    $scope.showPristineErrors = false;
-                    // guarantee correct scrolling for mobile
-                    $location.hash('step2');
-                    $anchorScroll();
-                } else {
-                    $scope.showPristineErrors = true;
-                }
-            };
-
-            /** Mark mobile wizard step 2 "done" - the ship-to address has been entered.*/
-            $scope.shipToDone = function (shipToFormValid, form) {
-                $scope.$broadcast('submitting:form', form);
-                // if the ship to form fields are hidden, angular considers them empty - work around that:
-                if (shipToFormValid || $scope.wiz.shipToSameAsBillTo) {
-                    $scope.wiz.step2Done = true;
-                    $scope.showPristineErrors = false;
-                    // guarantee correct scrolling for mobile
-                    $location.hash('step3');
-                    $anchorScroll();
-                } else {
-                    $scope.showPristineErrors = true;
-                }
-            };
-
-            /** Mark mobile wizard step 3 "done" - the payment information has been entered.*/
-            $scope.paymentDone = function (paymentFormValid, form) {
-                $scope.$broadcast('submitting:form', form);
-                if (paymentFormValid) {
-                    $scope.wiz.step3Done = true;
-                    // guarantee correct scrolling for mobile
-                    $location.hash('step4');
-                    $anchorScroll();
-                } else {
-                    $scope.showPristineErrors = true;
-                }
-
-            };
-
-            /** Mobile wizard - edit bill-do and mark subsequent steps as undone.*/
-            $scope.editBillTo = function () {
-                $scope.wiz.step1Done = false;
-                $scope.wiz.step2Done = false;
-                $scope.wiz.step3Done = false;
-            };
-
-            /** Mobile wizard - edit ship-to and mark subsequent steps as undone.*/
-            $scope.editShipTo = function () {
-                $scope.wiz.step2Done = false;
-                $scope.wiz.step3Done = false;
-            };
-
-            /** Mobile wizard - edit payment information.*/
-            $scope.editPayment = function () {
-                $scope.wiz.step3Done = false;
-            };
 
             /** Copy bill-to information to the ship-to properties.*/
             var setShipToSameAsBillTo = function () {
--- public/js/app/checkout/directives/mobile-checkout-wizard.js
+++ public/js/app/checkout/directives/mobile-checkout-wizard.js
@@ -16,13 +16,94 @@ angular.module('ds.checkout')
 /**
  * mobile-checkout-wizard
  *
- * this directive controls which checkout step is showing in the mobile view
+ * This directive includes a checkout wizard for the mobile checkout process, which requires that information
+ * is filled out in segments ("steps"), and all subsequent steps are blocked from input until the required
+ * information has been provided for previous steps. If the user edits a previously completed step, all subsequent steps
+ * are marked "undone" again, and the user has to tab through the remaining steps to advance.  Validation of
+ * required fields comes into play as the user attempts to advance to the next.  If there are missing fields,
+ * the missing fields will be highlighted as errors, and the user cannot advance until the necessary information
+ * has been provided.
  */
-    .directive('mobileCheckoutWizard',[function(){
+    .directive('mobileCheckoutWizard',['$location', '$anchorScroll', function($location, $anchorScroll){
         return {
             restrict: 'A',
             link: function(scope) {
+                var Wiz = function () {
+                    this.step1Done = false;
+                    this.step2Done = false;
+                    this.step3Done = false;
+                    this.shipToSameAsBillTo = true;
+                    // credit card expiration year drop-down - go 10 years out
+                    this.years = [];
+                    for (var year = new Date().getFullYear(), i = year, stop = year + 10; i < stop; i++) {
+                        this.years.push(i);
+                    }
+                    this.months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
 
+                };
+
+                scope.wiz = new Wiz();
+
+                /** Mark mobile wizard step 1 "done" - bill-to address information has been entered.*/
+                scope.billToDone = function (billToFormValid, form) {
+                    scope.$broadcast('submitting:form', form);
+                    if (billToFormValid) {
+                        scope.wiz.step1Done = true;
+                        scope.showPristineErrors = false;
+                        // guarantee correct scrolling for mobile
+                        $location.hash('step2');
+                        $anchorScroll();
+                    } else {
+                        scope.showPristineErrors = true;
+                    }
+                };
+
+                /** Mark mobile wizard step 2 "done" - the ship-to address has been entered.*/
+                scope.shipToDone = function (shipToFormValid, form) {
+                    scope.$broadcast('submitting:form', form);
+                    // if the ship to form fields are hidden, angular considers them empty - work around that:
+                    if (shipToFormValid || scope.wiz.shipToSameAsBillTo) {
+                        scope.wiz.step2Done = true;
+                        scope.showPristineErrors = false;
+                        // guarantee correct scrolling for mobile
+                        $location.hash('step3');
+                        $anchorScroll();
+                    } else {
+                        scope.showPristineErrors = true;
+                    }
+                };
+
+                /** Mark mobile wizard step 3 "done" - the payment information has been entered.*/
+                scope.paymentDone = function (paymentFormValid, form) {
+                    scope.$broadcast('submitting:form', form);
+                    if (paymentFormValid) {
+                        scope.wiz.step3Done = true;
+                        // guarantee correct scrolling for mobile
+                        $location.hash('step4');
+                        $anchorScroll();
+                    } else {
+                        scope.showPristineErrors = true;
+                    }
+
+                };
+
+                /** Mobile wizard - edit bill-do and mark subsequent steps as undone.*/
+                scope.editBillTo = function () {
+                    scope.wiz.step1Done = false;
+                    scope.wiz.step2Done = false;
+                    scope.wiz.step3Done = false;
+                };
+
+                /** Mobile wizard - edit ship-to and mark subsequent steps as undone.*/
+                scope.editShipTo = function () {
+                    scope.wiz.step2Done = false;
+                    scope.wiz.step3Done = false;
+                };
+
+                /** Mobile wizard - edit payment information.*/
+                scope.editPayment = function () {
+                    scope.wiz.step3Done = false;
+                };
             }
         };
     }]);
