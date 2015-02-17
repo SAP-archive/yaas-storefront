5d959ddc79fe2ef9af56cef36009693f6c1e9bfa
O'Donnell <mike.odonnell@hybris.com>
2015-02-17 14:16:39 -0700

    STOR-1460 add unit test stub

--- test/unit/shared/quantity-input-spec.js
+++ test/unit/shared/quantity-input-spec.js
@@ -12,7 +12,7 @@
 
 describe('quantityInput Test', function () {
 
-    var $scope, element;
+    var $scope, $compile;
 
     //***********************************************************************
     // Common Setup
@@ -22,21 +22,37 @@ describe('quantityInput Test', function () {
     // configure the target controller's module for testing - see angular.mock
     beforeEach(angular.mock.module('ds.shared'));
 
-    beforeEach(inject(function(_$rootScope_, _$compile_) {
-
-        this.addMatchers({
-            toEqualData: function (expected) {
-                return angular.equals(this.actual, expected);
-            }
-        });
-
-        element = angular.element(
-            '<input quantity-input ng-model="model.testinput" name="testinput"/>'
-        );
-
-        $scope =  _$rootScope_;
-        $scope.model = {};
-        _$compile_(element)($scope);
+    beforeEach(inject(function(_$rootScope_, _$compile_){
+        $scope = _$rootScope_;
+        $compile = _$compile_;
     }));
+
+    it('should be limited to numerics only', function () {
+
+        var inputTpl = angular.element('<div><input quantity-input ng-model="model.testinput" name="testinput"/></div>');
+        var element = $compile(inputTpl)($scope);
+        $scope.$digest();
+        var inputEl = element.find('input');
+        // inputEl.val('1');
+        console.log(inputEl.val());
+
+        var e = $.Event('keypress');
+        e.keyCode = 50;
+        e.which = 50;
+        inputEl.trigger(e);               //should update value
+
+        $scope.$digest();
+
+        inputEl = element.find('input');
+
+        console.log(inputEl.val());       //this value always blank
+
+        /*
+         the directive definitely does it's job but we are unable to get the model to update
+         in unit tests for some reason.  ignoring this for now
+         */
+        //expect(inputEl.val()).toEqual('the 2 key');
+
+    });
 
 });
