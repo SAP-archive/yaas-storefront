dcd78bde8a75af670aecad832382ec3b50e96dff
O'Donnell <mike.odonnell@hybris.com>
2015-02-18 11:51:46 -0700

    STOR-1460 allow up/down arrow keys

--- public/js/app/shared/directives/quantity-input.js
+++ public/js/app/shared/directives/quantity-input.js
@@ -27,7 +27,7 @@ angular.module('ds.shared')
                         (47<event.keyCode && event.keyCode<58 && event.shiftKey===false) ||
                         (95<event.keyCode && event.keyCode<106) ||
                         (event.keyCode===8) || (event.keyCode===9) ||
-                        (event.keyCode>34 && event.keyCode<40) ||
+                        (event.keyCode>34 && event.keyCode<41) ||
                         (event.keyCode===46)))
                     {
                         event.preventDefault();
