dba0709af9e5440b36fda285e91fd50bbb22dec3
Nemanja Popovic <nemanja.popovic01@sap.com>
2015-02-19 14:16:56 +0100

    Breadcrumb size implementation

--- public/js/app/shared/directives/y-breadcrumb.js
+++ public/js/app/shared/directives/y-breadcrumb.js
@@ -15,15 +15,21 @@
 angular.module('ds.ybreadcrumb', [])
     .run(['$templateCache', function($templateCache) {
         $templateCache.put('template/y-breadcrumb.html',
-            '<div class=\"row productListingBreadCrumb\" ng-cloak>' +
-                '<div ng-if=\"items.name\" class=\"col-xs-12 col-sm-6  title breadCrumbText\">' +
-                    '<a ng-repeat=\"item in items.path\" ng-if=\"!$last\" ui-sref=\"base.category({catName: item.slug})\" id=\"bc{{item.id}}\">' +
-                        '<span ng-if=\"!$first\"> / </span> {{item.name}}' +
-                    '</a>' +
-                '<span ng-if=\"items.path && items.path.length > 1\"> / </span>' +
-                '{{items.path[items.path.length -1].name}}' +
+            '<div>' +
+                '<div ng-show=\"size !== \'small\'\" class=\"row productListingBreadCrumb\" ng-cloak>' +
+                    '<div ng-if=\"items.name\" class=\"col-xs-12 col-sm-6  title breadCrumbText\">' +
+                        '<a ng-repeat=\"item in items.path\" ng-if=\"!$last\" ui-sref=\"base.category({catName: item.slug})\" id=\"bc{{item.id}}\">' +
+                            '<span ng-if=\"!$first\"> / </span> {{item.name}}' +
+                        '</a>' +
+                        '<span ng-if=\"items.path && items.path.length > 1\"> / </span>' +
+                        '{{items.path[items.path.length -1].name}}' +
+                    '</div>' +
                 '</div>' +
-            '</div>');
+                '<small  ng-show=\"size === \'small\'\"><a ng-repeat=\"item in items.path\" ui-sref=\"base.category({catName: item.slug})\" id=\"bc{{item.id}}\">' +
+                    '<span ng-if=\"!$first\">/</span> {{item.name}}' +
+                '</a></small>' +
+            '</div>'
+        );
     }]);
 
 angular.module('ds.ybreadcrumb')
