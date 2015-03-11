e206d3ee8832ee0a53ae5ba09395ea63273f45fc
Clint Ashworth <clintonnash44@gmail.com>
2015-03-11 14:47:06 -0600

    STOR-1412: new coupon files.

---
+++ public/js/app/coupons/services/coupon-user.js
@@ -0,0 +1,63 @@
+/**
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ */
+
+'use strict';
+
+/**
+ *  Users singleton Coupon object.
+ */
+angular.module('ds.coupon')
+    .factory('UserCoupon', [ '$rootScope', function( $rootScope ){
+
+    	var userCoupon = {};
+		var blankCoupon = {
+            code: '',
+            applied: false,
+            valid: true,
+			message : {
+				error: "Code not valid",
+				success: "Applied"
+			},
+			amounts : {
+                // shippingAmount: 0,
+                originalAmount: 0,
+                // originalTotalAmount: 0,
+                // redeemDiscount: 0,
+                // couponDiscount: 0,
+                discountAmount: 0//,
+                // newAmount: 0
+            }
+        };
+        userCoupon = angular.copy(blankCoupon);
+
+        function updateCoupons() {
+        	debugger;
+			$rootScope.$broadcast("couponUpdated", userCoupon);
+  		};
+
+
+
+    	return {
+    		getCoupon:function(){
+    			return userCoupon;
+    		},
+    		setCoupon:function(couponData){
+    			updateCoupons();
+    			return angular.extend(userCoupon, couponData);
+    		},
+    		setBlankCoupon:function(){
+    			userCoupon = angular.copy(blankCoupon);
+    			updateCoupons();
+    		}
+   		 };
+
+    }]);
