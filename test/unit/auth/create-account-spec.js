d038f13b3dba03884b0f87d3b8431a01d348c2ae
Clint Ashworth <clintonnash44@gmail.com>
2015-04-09 14:45:13 -0600

    STOR-865: tests

--- test/unit/auth/create-account-spec.js
+++ test/unit/auth/create-account-spec.js
@@ -10,59 +10,34 @@
  * license agreement you entered into with hybris.
  */
 
-ddescribe('directive: create account', function() {
-    var scope,
-        elem,
-        directive,
-        compiled,
-        html;
+describe('handle account creation panel', function(){
+    var $compile, $rootScope;
+    var mockedSettings = {};
+    var mockedAuthSvc = {
+        requestPasswordReset: jasmine.createSpy('requestPasswordReset').andCallFake(function(){
+            return deferredPwReset.promise;
+        }),
+        changePassword: jasmine.createSpy('changePassword').andCallFake(function(){
+            return deferredChangePassword.promise;
+        })
+    }
 
-    // beforeEach(function (){
-    //     module('ds.auth');
-
-    //     elem = angular.element('<div ng-include=\"\'js/app/auth/templates/create-account.html\'\" create-account ></div>');
-
-    //     inject(function($compile, $rootScope) {
-    //         scope = $rootScope.$new();
-
-    //         compiled = $compile(elem);
-
-    //         compiled(scope);
-    //     });
-    // });
-
-    beforeEach(inject(function(_$rootScope_, _$compile_) {
-
-        this.addMatchers({
-            toEqualData: function (expected) {
-                return angular.equals(this.actual, expected);
-            }
-        });
-
-        element = angular.element('<div ng-include=\"\'js/app/auth/templates/create-account.html\'\" create-account ></div>');
-
-        $rootScope =  _$rootScope_;
-        _$compile_(element)($rootScope);
-    }));
-
-    describe('handle account creation signup', function(){
-
-        it('should call createCartTimeout and close the cart after 3 seconds', function(){
-            $rootScope.showCart = true;
-            $rootScope.$emit('cart:closeAfterTimeout');
-            $rootScope.$digest();
-            setTimeout(function() {
-                expect($rootScope.showCart).toEqualData(false);
-            },4000);
-            $rootScope.$digest();
-        });
-
-        it('should close the cart now', function(){
-            $rootScope.showCart = true;
-            $rootScope.$emit('cart:closeNow');
-            expect($rootScope.showCart).toEqualData(false);
-        });
+    module(function($provide){
+        module('ds.auth');
+        $provide.value('AuthSvc', mockedAuthSvc);
+        $provide.value('settings', mockedSettings);
     });
 
+    beforeEach(inject(function(_$compile_, _$rootScope_){
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('should access the directive members signup', function() {
+        var element = $compile("<div create-account ></div>")($rootScope);
+        $rootScope.$digest();
+        expect($rootScope.signup).toBeDefined();
+    });
 
 });
+
