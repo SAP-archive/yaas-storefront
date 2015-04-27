8e548f9a2126d382392e3e4e199a2770e76c6bd3
Clint Ashworth <clintonnash44@gmail.com>
2015-04-27 10:24:18 -0600

    STOR-1593: first working https

--- server.js
+++ server.js
@@ -5,16 +5,27 @@ var express = require('express');
 var path = require('path');
 var request = require('request');
 
+var fs = require('fs');
+var https = require('https');
+var https_options = {
+  key: fs.readFileSync('server/keys/key.pem'),
+  cert: fs.readFileSync('server/keys/cert.pem')
+};
+
+
 // Build the server
 var app = express();
 
 // map access to public files
 app.use(express.static(__dirname + '/public'));
 
+console.log('STARTING HTTPS SERVER.');
+
+// create https service
+https.createServer(https_options, app).listen(9000);
+
+console.log('HTTPS SERVER STARTED.');
+
 module.exports = app;
-
-
-
-
 
 
