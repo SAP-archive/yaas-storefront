c586de7c2011ea27fcbc34ec39b1333506869c06
Nash <clintonnash44@gmail.com>
2015-05-04 10:53:11 -0600

    Update app-config.js
    Default changes and revert to example.com in redirect URI.

--- public/js/app/shared/app-config.js
+++ public/js/app/shared/app-config.js
@@ -35,19 +35,19 @@ angular.module('ds.appconfig', [])
                 tenantId = window.location.pathname.substring( 1, pathLength-1 );
             } else {
                 // Dynamic ProjectId is configured and replaced by build script, see gruntfile.
-                tenantId = /*StartProjectId*/ 'bsdtestproject' /*EndProjectId*/;
+                tenantId = /*StartProjectId*/ 'defaultproj' /*EndProjectId*/;
             }
             return tenantId;
         },
 
         clientId: function() {
             // Dynamic ClientId is configured and replaced by build script, see gruntfile.
-            return /*StartClientId*/ 'ST8laHxt59OvvYwDGrJE8bviaES0L7CB' /*EndClientId*/;
+            return /*StartClientId*/ 'i9nUtOWlGwALS2oERqRFPZznDKShF2B9' /*EndClientId*/;
         },
 
         redirectURI: function() {
             // Dynamic RedirectURI is configured and replaced by build script, see gruntfile.
-            return /*StartRedirectURI*/ 'http://google.com' /*EndRedirectURI*/;
+            return /*StartRedirectURI*/ 'http://example.com' /*EndRedirectURI*/;
         }
 
 
