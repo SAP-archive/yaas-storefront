57f87d071d2fe182cd4558ad2b52c38f1ef25376
Jeremy Rawls <jeremy.rawls@hybris.com>
2015-05-07 15:49:33 -0500

    adding orer date

--- public/js/app/shared/app-config.js
+++ public/js/app/shared/app-config.js
@@ -35,14 +35,14 @@ angular.module('ds.appconfig', [])
                 tenantId = window.location.pathname.substring( 1, pathLength-1 );
             } else {
                 // Dynamic ProjectId is configured and replaced by build script, see gruntfile.
-                tenantId = /*StartProjectId*/ 'defaultproj' /*EndProjectId*/;
+                tenantId = /*StartProjectId*/ 'bsdtestproject' /*EndProjectId*/;
             }
             return tenantId;
         },
 
         clientId: function() {
             // Dynamic ClientId is configured and replaced by build script, see gruntfile.
-            return /*StartClientId*/ 'i9nUtOWlGwALS2oERqRFPZznDKShF2B9' /*EndClientId*/;
+            return /*StartClientId*/ 'ST8laHxt59OvvYwDGrJE8bviaES0L7CB' /*EndClientId*/;
         },
 
         redirectURI: function() {
