b04b31956da4bcade6c97e50ca5b50da141c8ea7
Nemanja Popovic <nemanja.popovic01@sap.com>
2015-06-30 12:06:11 +0200

    Return app-config to default proj.

--- public/js/app/shared/app-config.js
+++ public/js/app/shared/app-config.js
@@ -35,14 +35,14 @@ angular.module('ds.appconfig', [])
                 tenantId = window.location.pathname.substring( 1, pathLength-1 );
             } else {
                 // Dynamic ProjectId is configured and replaced by build script, see gruntfile.
-                tenantId = /*StartProjectId*/ 'sitesettingsproj' /*EndProjectId*/;
+                tenantId = /*StartProjectId*/ 'defaultproj' /*EndProjectId*/;
             }
             return tenantId;
         },
 
         clientId: function() {
             // Dynamic ClientId is configured and replaced by build script, see gruntfile.
-            return /*StartClientId*/ '22skxZtptvePdpLjmugv9hrujKyKrMaS' /*EndClientId*/;
+            return /*StartClientId*/ 'i9nUtOWlGwALS2oERqRFPZznDKShF2B9' /*EndClientId*/;
         },
 
         redirectURI: function() {
