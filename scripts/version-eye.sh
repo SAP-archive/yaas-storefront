15c8dade661fcd2fec82522ea2f724f93f04581b
Mike Nightingale <mike.nightingale@hybris.com>
2015-08-24 11:23:38 -0600

    fixed version-eye syntax STOR-1853

--- scripts/version-eye.sh
+++ scripts/version-eye.sh
@@ -8,8 +8,8 @@
 BASE_DIR=`dirname $0`
 DEST_FILE=$BASE_DIR/result.json
 
-function jsonval {
-    temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
+jsonval () {
+    temp="echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop"
     echo ${temp##*|}
 }
 
