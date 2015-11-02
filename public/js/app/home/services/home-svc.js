1a3efa731daf5c31296d6108669a7adb22548c1e
Nemanja Popovic <nemanja.popovic01@sap.com>
2015-11-02 17:22:40 +0100

    Small code refactor and testing of site content on storefront.

---
+++ public/js/app/home/services/home-svc.js
@@ -0,0 +1,108 @@
+﻿/**
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('ds.home')
+        .service('HomeSvc', ['GlobalData', 'settings', '$state',
+            function (GlobalData, settings, $state) {
+
+                var homeBannerId = 'homeBanner';
+
+                /**
+                 * Initializes all site content in objects that can be used in UI
+                 * @return object that contains carousel images and banners
+                 */
+                var init = function init() {
+                    var self = this;
+                    var slidesLarge = [];
+                    var slidesSmall = [];
+                    var banner1 = {};
+                    var banner2 = {};
+                    var siteContent = GlobalData.getSiteBanners();
+
+                    if (self.siteContentExists(siteContent)) {
+                        if (!!siteContent.topImages && siteContent.topImages.length > 0) {
+
+                            for (var i = 0; i < siteContent.topImages.length; i++) {
+                                if (!!siteContent.topImages[i].large && siteContent.topImages[i].large.imageUrl !== '') {
+                                    slidesLarge.push({ id: homeBannerId, image: siteContent.topImages[i].large });
+                                }
+
+                                if (!!siteContent.topImages[i].small && siteContent.topImages[i].small.imageUrl !== '') {
+                                    slidesSmall.push({ id: homeBannerId, image: siteContent.topImages[i].small });
+                                }
+                            }
+                        }
+                        banner1 = siteContent.banner1;
+                        banner2 = siteContent.banner2;
+                    }
+                    else {
+                        //Redirect to all products page
+                        $state.go(settings.allProductsState);
+                    }
+
+                    return {
+                        slidesLarge: slidesLarge,
+                        slidesSmall: slidesSmall,
+                        banner1: banner1,
+                        banner2: banner2
+                    };
+                };
+
+                /**
+                 * Checks if there is any site content defined for selected site
+                 * @param siteContent - current site content
+                 * @return true/false value that shows if there is site content for provided site
+                 */
+                var siteContentExists = function siteContentExists(siteContent) {
+                    if (!siteContent) {
+                        return false;
+                    }
+
+                    if (!!siteContent.topImages) {
+                        for (var i = 0; i < siteContent.topImages.length; i++) {
+                            if (!!siteContent.topImages[i] &&
+                                !!siteContent.topImages[i].large &&
+                                (siteContent.topImages[i].large.imageUrl !== '' ||
+                                 siteContent.topImages[i].small.imageUrl !== '')) {
+                                return true;
+                            }
+                        }
+                    }
+
+                    if (!!siteContent.banner1 &&
+                        !!siteContent.banner1.large &&
+                        (siteContent.banner1.large.imageUrl !== '' ||
+                         siteContent.banner1.small.imageUrl !== '')) {
+                        return true;
+                    }
+
+                    if (!!siteContent.banner2 && !!
+                        !!siteContent.banner2.large &&
+                        (siteContent.banner2.large.imageUrl !== '' ||
+                         siteContent.banner2.small.imageUrl !== '')) {
+                        return true;
+                    }
+
+                    return false;
+                };
+
+                return {
+                    init: init,
+                    siteContentExists: siteContentExists
+                };
+
+            }]);
+
+})();
---
+++ test/unit/home/home-svc-spec.js
@@ -0,0 +1,197 @@
+﻿/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ */
+
+describe('HomeSvc Test', function () {
+
+    var $scope, $rootScope, $controller, homeSvc, mockedGlobalData, mockedSettings, mockedState;
+
+    mockedGlobalData = {};
+    mockedSettings = {};
+    mockedState = {
+        go: jasmine.createSpy()
+    };
+
+    beforeEach(function () {
+        module('ds.home', function ($provide) {
+            $provide.value('appConfig', {});
+            $provide.value('GlobalData', mockedGlobalData);
+            $provide.value('$state', mockedState);
+        });
+    });
+
+    beforeEach(inject(function (_$rootScope_, _$controller_, _$q_, _HomeSvc_) {
+
+        this.addMatchers({
+            toEqualData: function (expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
+        $rootScope = _$rootScope_;
+        $scope = _$rootScope_.$new();
+        $controller = _$controller_;
+        homeSvc = _HomeSvc_;
+
+        console.warn(homeSvc);
+    }));
+
+    describe('Interface', function () {
+
+        it('should expose correct interface', function () {
+            expect(homeSvc.init).toBeDefined();
+            expect(homeSvc.siteContentExists).toBeDefined();
+        });
+
+    });
+
+    describe('Init()', function () {
+        it('should check if site content exists and if not should redirect to all products page', function () {
+            mockedGlobalData.getSiteBanners = jasmine.createSpy().andReturn({});
+
+            homeSvc.init();
+
+            expect(mockedState.go).toHaveBeenCalled();
+        });
+
+        it('should check if site content exists and if yes it should return object with all details', function () {
+            mockedGlobalData.getSiteBanners = jasmine.createSpy().andReturn({
+                topImages: [{
+                    large: {
+                        imageUrl: 'imgLarge1', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: 'imgSmall1', hyperlinkUrl: ''
+                    }
+                }, {
+                    large: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    }
+                }],
+                banner1: {
+                    large: {
+                        imageUrl: 'banner1Large', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: 'banner1Small', hyperlinkUrl: ''
+                    }
+                }
+            });
+
+            var returnObj = homeSvc.init();
+
+            expect(returnObj.slidesLarge).toEqualData([{ id: 'homeBanner', image: { imageUrl: 'imgLarge1', hyperlinkUrl: '' } }]);
+            expect(returnObj.slidesSmall).toEqualData([{ id: 'homeBanner', image: { imageUrl: 'imgSmall1', hyperlinkUrl: '' } }]);
+            expect(returnObj.banner1).not.toEqualData({});
+            expect(returnObj.banner2).not.toBeDefined();
+
+
+            mockedGlobalData.getSiteBanners = jasmine.createSpy().andReturn({
+                banner1: {
+                    large: {
+                        imageUrl: 'banner1Large', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: 'banner1Small', hyperlinkUrl: ''
+                    }
+                }
+            });
+
+            var returnObj = homeSvc.init();
+
+            expect(returnObj.slidesLarge).toEqualData([]);
+            expect(returnObj.slidesSmall).toEqualData([]);
+            expect(returnObj.banner1).not.toEqualData({});
+            expect(returnObj.banner2).not.toBeDefined();
+        });
+    });
+
+    describe('siteContentExist()', function () {
+
+        it('should return false if site content is not defined', function () {
+            var result = homeSvc.siteContentExists();
+            expect(result).toBe(false);
+        });
+
+        it('should check if top images are defined and return true if there is at least one image defined there', function () {
+            var result = homeSvc.siteContentExists({
+                topImages:[{
+                    large: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: 'banner1Small', hyperlinkUrl: ''
+                    }
+                }]
+            });
+            expect(result).toBe(true);
+
+            result = homeSvc.siteContentExists({
+                topImages: [{
+                    large: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    }
+                }, {
+                    large: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: 'banner1Small', hyperlinkUrl: ''
+                    }
+                }]
+            });
+            expect(result).toBe(true);
+
+            result = homeSvc.siteContentExists({
+                banner1: {
+                    large: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: 'smallUrl', hyperlinkUrl: ''
+                    }
+                }
+            });
+            expect(result).toBe(true);
+
+
+            result = homeSvc.siteContentExists({
+                banner2: {
+                    large: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: 'banner2Small', hyperlinkUrl: ''
+                    }
+                }
+            });
+            expect(result).toBe(true);
+
+            result = homeSvc.siteContentExists({
+                banner2: {
+                    large: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    },
+                    small: {
+                        imageUrl: '', hyperlinkUrl: ''
+                    }
+                }
+            });
+            expect(result).toBe(false);
+        });
+    });
+
+});
