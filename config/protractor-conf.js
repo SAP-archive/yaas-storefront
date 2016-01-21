12f3c625c01b3085fc6ae3fbf6148c2349a00dd4
Dana Roibu <dana.roibu@hybris.com>
2016-01-21 11:25:19 +0100

    Temporary commit for testing errors on Saucelabs

--- config/protractor-conf.js
+++ config/protractor-conf.js
@@ -8,11 +8,11 @@ exports.config = {
 
     specs: [
 
-        '../test/e2e/cart-tests.js',
-        '../test/e2e/checkout-tests.js',
+        //'../test/e2e/cart-tests.js',
+        //'../test/e2e/checkout-tests.js',
         '../test/e2e/coupon-tests.js',
-        '../test/e2e/login-tests.js',
-        '../test/e2e/product-tests.js'
+        //'../test/e2e/login-tests.js',
+        //'../test/e2e/product-tests.js'
     ],
 
 
