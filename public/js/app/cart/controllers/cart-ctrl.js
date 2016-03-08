4aaa5154021511f630db46ff131a665b724d0a27
Raphi Stein <raphael.stein@sap.com>
2016-03-08 14:21:55 -0500

    completed UI work on Add Note mixin. Next is to turn into directive and implement Save functionality

--- public/js/app/cart/controllers/cart-ctrl.js
+++ public/js/app/cart/controllers/cart-ctrl.js
@@ -16,91 +16,115 @@ angular.module('ds.cart')
     /** This controller manages the interactions of the cart view. The controller is listening to the 'cart:udpated' event
      * and will refresh the scope's cart instance when the event is received. */
     .controller('CartCtrl', ['$scope', '$state', '$rootScope', 'CartSvc', 'GlobalData', 'settings', 'AuthSvc', 'AuthDialogManager',
-            function($scope, $state, $rootScope, CartSvc, GlobalData, settings, AuthSvc, AuthDialogManager) {
+        function($scope, $state, $rootScope, CartSvc, GlobalData, settings, AuthSvc, AuthDialogManager) {
 
-        $scope.cart = CartSvc.getLocalCart();
-        $scope.currencySymbol = GlobalData.getCurrencySymbol($scope.cart.currency);
-
-        $scope.showTaxEstimation = false;
-
-        $scope.taxConfiguration = GlobalData.getCurrentTaxConfiguration();
-      
-        $scope.couponCollapsed = true;
-        $scope.taxType = GlobalData.getTaxType();
-
-        $scope.calculateTax = CartSvc.getCalculateTax();
-
-        var unbind = $rootScope.$on('cart:updated', function(eve, eveObj){
-            $scope.cart = eveObj.cart;
+            $scope.cart = CartSvc.getLocalCart();
             $scope.currencySymbol = GlobalData.getCurrencySymbol($scope.cart.currency);
-            $scope.taxType = GlobalData.getTaxType();
+
+            $scope.showTaxEstimation = false;
+
             $scope.taxConfiguration = GlobalData.getCurrentTaxConfiguration();
+
+            $scope.couponCollapsed = true;
+            $scope.taxType = GlobalData.getTaxType();
+
             $scope.calculateTax = CartSvc.getCalculateTax();
-        });
 
-        $scope.$on('$destroy', unbind);
+            var unbind = $rootScope.$on('cart:updated', function(eve, eveObj) {
+                $scope.cart = eveObj.cart;
+                $scope.currencySymbol = GlobalData.getCurrencySymbol($scope.cart.currency);
+                $scope.taxType = GlobalData.getTaxType();
+                $scope.taxConfiguration = GlobalData.getCurrentTaxConfiguration();
+                $scope.calculateTax = CartSvc.getCalculateTax();
+            });
 
-        /** Remove a product from the cart.
-         * @param cart item id
-         * */
-        $scope.removeProductFromCart = function (itemId) {
-            CartSvc.removeProductFromCart(itemId);
-        };
+            $scope.$on('$destroy', unbind);
 
-        /** Toggles the "show cart view" property.
-         */
-        $scope.toggleCart = function (){
-            $rootScope.showCart = false;
-        };
+            /** Remove a product from the cart.
+             * @param cart item id
+             * */
+            $scope.removeProductFromCart = function(itemId) {
+                CartSvc.removeProductFromCart(itemId);
+            };
 
-        /**
-         *  Issues an "update cart" call to the service or removes the item if the quantity is undefined or zero.
-         */
-        $scope.updateCartItemQty = function (item, itemQty, config) {
-            if (itemQty > 0) {
-                CartSvc.updateCartItemQty(item, itemQty, config);
-            }
-            else if (!itemQty || itemQty === 0) {
-                CartSvc.removeProductFromCart(item.id);
-            }
-        };
+            /** Toggles the "show cart view" property.
+             */
+            $scope.toggleCart = function() {
+                $rootScope.showCart = false;
+            };
 
-        $scope.toCheckoutDetails = function () {
-            $scope.keepCartOpen();
-            if (!AuthSvc.isAuthenticated()) {
-                var dlg = AuthDialogManager.open({windowClass:'mobileLoginModal'}, {}, {}, true);
+            /**
+             *  Issues an "update cart" call to the service or removes the item if the quantity is undefined or zero.
+             */
+            $scope.updateCartItemQty = function(item, itemQty, config) {
+                if (itemQty > 0) {
+                    CartSvc.updateCartItemQty(item, itemQty, config);
+                }
+                else if (!itemQty || itemQty === 0) {
+                    CartSvc.removeProductFromCart(item.id);
+                }
+            };
 
-                dlg.then(function(){
+            $scope.toCheckoutDetails = function() {
+                $scope.keepCartOpen();
+                if (!AuthSvc.isAuthenticated()) {
+                    var dlg = AuthDialogManager.open({ windowClass: 'mobileLoginModal' }, {}, {}, true);
+
+                    dlg.then(function() {
                         if (AuthSvc.isAuthenticated()) {
                             $state.go('base.checkout.details');
                         }
                     },
-                    function(){
+                        function() {
 
+                        }
+                    );
+                }
+                else {
+                    $state.go('base.checkout.details');
+                }
+            };
+
+            $scope.applyTax = function() {
+                $scope.taxEstimationError = false;
+                if ($scope.calculateTax.countryCode !== '' && $scope.calculateTax.zipCode !== '') {
+                    //Save countryCode and zipCode in service
+                    CartSvc.setCalculateTax($scope.calculateTax.zipCode, $scope.calculateTax.countryCode, $scope.cart.id);
+
+                    $scope.calculateTax.taxCalculationApplied = true;
+
+                }
+                else {
+                    //Show error message
+                    $scope.calculateTax.taxCalculationApplied = false;
+                    $scope.showTaxEstimation = false;
+                    $scope.taxEstimationError = true;
+                }
+
+            };
+
+            // Add NOTE mixin
+            $scope.note = {
+                noteCollapsed: true,
+                content: "",
+                onBlur: function(){
+                    if (this.content == ""){
+                        this.collapseNote();
                     }
-                );
-            }
-            else {
-                $state.go('base.checkout.details');
-            }
-        };
-
-        $scope.applyTax = function () {
-            $scope.taxEstimationError = false;
-            if ($scope.calculateTax.countryCode !== '' && $scope.calculateTax.zipCode !== '') {
-                //Save countryCode and zipCode in service
-                CartSvc.setCalculateTax($scope.calculateTax.zipCode, $scope.calculateTax.countryCode, $scope.cart.id);
-
-                $scope.calculateTax.taxCalculationApplied = true;
-
-            }
-            else {
-                //Show error message
-                $scope.calculateTax.taxCalculationApplied = false;
-                $scope.showTaxEstimation = false;
-                $scope.taxEstimationError = true;
+                    else {
+                        // save first and then collapseNote
+                    }
+                },
+                collapseNote: function() {
+                    this.noteCollapsed = true;
+                },
+                expandNote: function() {
+                    this.noteCollapsed = false;
+                },
+                submit: function(){
+                    alert(this.content);
+                    alert("ERROR: Save functionality not yet implemented");
+                }
             }
 
-        };
-
-    }]);
+        }]);
